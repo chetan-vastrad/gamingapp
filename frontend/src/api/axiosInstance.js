e07904ef9e27e5ba453c287c8ae07b485f757d80
chetannumasoft <chetannumasoft@gmail.com>
2025-11-11 16:33:49 +0530

    frontend

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://gamingapp-f72a.onrender.com/api",
 });
 
 export default axiosInstance;
